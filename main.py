677e77bac5e4d9326d82ba3f6d8fc537ea4a39fe
lopezdario98 <116035252+lopezdario98@users.noreply.github.com>
2023-01-28 02:09:44 -0300

    Primera Subida
    Estos es para ver como funciona Github

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Saludos humano!")
+
+nombre = input("Por favor, introduzca su nombre: ")
+
+if:
+    #Si aca ponen nombres como "shony, yoni, yeni, kevin, braian, kimbrely"
+    print("Que onda perro!")
+
+else: 
+    #Si aca ponen otros nombres.
+    print("")
